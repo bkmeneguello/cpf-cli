ad25a5f42f1646a8499b44b696c1d7be8469441a
Matheus Alves <theuves@outlook.com>
2017-03-24 21:12:47 -0400

    várias alterações

--- cli.js
+++ cli.js
@@ -1,84 +1,67 @@
 #!/usr/bin/env node
+"use strict";
 
-'use strict';
+// cpf-cli | matheus alves
+// https://github.com/theuves/cpf-cli
 
-const CPF = require('cpf');
-
-const help = `
-  Uso: cpf [opções] <número>
-
-  Opções:
-
-    -f, --formatar  formata CPF
-    -v, --versão    exibe a versão
-    -?, --ajuda     exibe essa mensagem
-`;
-
-const validOpts = [
-    '-f', '--formatar'
-    '-v', '--versão',
-    '-?', '--ajuda',
-];
+var CPF = require("cpf");
 
 var format = false;
-var args = process.argv.slice(2);
 
-for (var i in args) {
-    if (/^--?/.test(args[i])) {
-        if (validOpts.indexOf(args[i]) !== -1) {
-            switch (args[i]) {
-                case '-v':
-                case '--versão':
-                    console.log(`v${require('./package.json').version}`);
-                    process.exit(0);
-                case '-?':
-                case '--ajuda':
-                    console.log(help);
-                    process.exit(0);
-                case '-f':
-                case '--formatar':
-                    format = true;
-                    break;
-            }
-        } else {
-            console.error(`A opção "${args[i]}" não é válida.`);
-            process.exit(1);
-        }
+process.argv.slice(2).forEach(function (arg) {
+  if (/^--?/.test(arg)) {
+    if ([
+      "-f", "--formatar",
+      "-v", "--versão",
+      "-?", "--ajuda"
+    ].includes(arg)) {
+      if (/^(-v|--versão)$/.test(arg)) {
+        console.log("v" + require("./package.json").version);
+        process.exit(0);
+      } else if (/^(-\?|--ajuda)$/.test(arg)) {
+        console.log([
+          "",
+          "  Uso: cpf [opções] <número>",
+          "",
+          "  Opção:          Descrição:",
+          "  -f, --formatar  Formatar número.",
+          "  -v, --versão    Exibir a versão atual.",
+          "  -?, --ajuda     Exibir esta mensagem.",
+          "",
+        ].join("\n"));
+        process.exit(0);
+      } else if (/^(-f|--formatar)$/.test(arg)) {
+        format = true;
+      }
+    } else {
+      console.error("Opção Inválida.");
+      process.exit(1);
     }
-}
-
-args = args.filter(i => {
-    if (!/^(-f|--formatar)$/.test(i)) {
-        return i;
-    }
+  }
 });
 
 if (process.argv.length === 2) {
-    console.log(CPF.generate());
+  console.log(CPF.generate());
 } else if (!format || process.argv.length === 3) {
-    const num = process.argv[2], val = CPF.validate(num);
-
-    if (/^--?/.test(num)) {
-        console.log(CPF.generate());
-        process.exit(0);
-    }
-
-    console.log(val ? 'Válido' : 'Inválido');
+  if (/^--?/.test(process.argv[2])) {
+    console.log(CPF.generate());
+  } else {
+    console.log(CPF.validate(process.argv[2])
+      ? "Válido."
+      : "Inválido.");
+  }
 } else {
-    const arg1 = process.argv[2],
-        arg2 = process.argv[3],
-        arg = /^(-f|--formatar)$/.test(arg1) ? arg2 : arg1;
+  var arg = /^(-f|--formatar)$/.test(process.argv[2])
+    ? process.argv[3]
+    : process.argv[2];
 
-    if (/^--?/.test(arg)) {
-        console.log(CPF.generate());
-    } else {
-        const formatted = CPF.format(arg);
+  if (/^--?/.test(arg)) {
+    console.log(CPF.generate());
+  } else {
+    var formatted = CPF.format(arg);
 
-        if (!formatted) {
-            console.log('Inválido');
-            process.exit(0);
-        }
-
-        console.log(formatted);
-    }
+    console.log(formatted
+      ? formatted
+      : "Inválido.");
+  }
 }
